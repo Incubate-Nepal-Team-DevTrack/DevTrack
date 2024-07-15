658d17f96faa19915d0699d42e2b70a831917e63
sushilbhattarai45 <bhattaraisushil005@gmail.com>
2024-07-15 13:16:20 +0545

    Search done

--- src/app/components/sideNav.tsx
+++ src/app/components/sideNav.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { Input } from "@/components/ui/input";
 
 const SideNav = () => {
-  const data = {
+  const initialData = {
     projects: [
       {
         name: "Melamchi Water Project",
@@ -64,8 +64,22 @@ const SideNav = () => {
     ],
   };
   const [selectedProject, setSelectedProject] = useState<string>("");
+  const [duplicateDta, setDuplicateData] = useState(initialData);
+
+  async function Search(value: string) {
+    console.log(value);
+    const data = initialData.projects.filter((project) =>
+      project.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setDuplicateData({
+      projects: data,
+    });
+  }
   return (
     <aside
+      style={{
+        height: "100vh",
+      }}
       id="logo-sidebar"
       className="fixed top-0 left-0 z-40 w-65 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 "
     >
@@ -79,6 +93,9 @@ const SideNav = () => {
           }}
         >
           <Input
+            onChange={(e) => {
+              Search(e.target.value);
+            }}
             placeholder="Search Projects"
             style={{
               height: 50,
@@ -91,9 +108,119 @@ const SideNav = () => {
             marginTop: 50,
           }}
         >
-          {data.projects.map((project) => (
+          {duplicateDta.projects.length !== 0 ? (
+            duplicateDta.projects.map((project) => (
+              <li
+                onClick={() => setSelectedProject(project.name)}
+                style={{
+                  width: 300,
+                }}
+              >
+                <div
+                  style={{
+                    // backgroundColor:
+                    //   selectedProject === project.name ? "#603CC7" : "white",
+                    borderWidth: selectedProject === project.name ? 2 : 0,
+                    borderColor:
+                      selectedProject === project.name ? "#603CC7" : "",
+                    borderStyle: "dashed",
+                  }}
+                  className="max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <div>
+                    <div
+                      style={{
+                        flex: 1,
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      <h5
+                        style={{
+                          flex: 0.9,
+                        }}
+                        className="mb-0 text1xl font-bold tracking-tight text-gray-900 dark:text-white"
+                      >
+                        {project.name}
+                      </h5>
+                      <div
+                        style={{
+                          flex: 0.1,
+                        }}
+                      >
+                        <div
+                          style={{
+                            alignSelf: "flex-end",
+                            height: 15,
+                            width: 15,
+                            borderRadius: 10,
+                            backgroundColor: project.abandoned
+                              ? "red"
+                              : "#1EA362",
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+
+                    <div
+                      className="mb-1"
+                      style={{
+                        display: "flex",
+                        justifyContent: "start",
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontSize: "0.8rem",
+                          color: "gray",
+                          marginRight: "10px",
+                        }}
+                      >
+                        {project.date}
+                      </span>
+                      <span
+                        style={{
+                          fontSize: "0.8rem",
+                          color: "gray",
+                          marginRight: "10px",
+                        }}
+                      >
+                        {project.status}
+                      </span>
+                    </div>
+                    <div
+                      className="mb-1"
+                      style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "start",
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontSize: "0.8rem",
+                          color: "gray",
+                          marginRight: "10px",
+                        }}
+                      >
+                        {project.contractor}
+                      </span>
+                      <span
+                        style={{
+                          fontSize: "0.8rem",
+                          color: "gray",
+                          marginRight: "10px",
+                        }}
+                      >
+                        {project.location}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))
+          ) : (
             <li
-              onClick={() => setSelectedProject(project.name)}
               style={{
                 width: 300,
               }}
@@ -102,105 +229,15 @@ const SideNav = () => {
                 style={{
                   // backgroundColor:
                   //   selectedProject === project.name ? "#603CC7" : "white",
-                  borderWidth: selectedProject === project.name ? 2 : 0,
-                  borderColor:
-                    selectedProject === project.name ? "#603CC7" : "",
+
                   borderStyle: "dashed",
                 }}
                 className="max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
               >
-                <div>
-                  <div
-                    style={{
-                      flex: 1,
-                      display: "flex",
-                      alignItems: "center",
-                    }}
-                  >
-                    <h5
-                      style={{
-                        flex: 0.9,
-                      }}
-                      className="mb-0 text1xl font-bold tracking-tight text-gray-900 dark:text-white"
-                    >
-                      {project.name}
-                    </h5>
-                    <div
-                      style={{
-                        flex: 0.1,
-                      }}
-                    >
-                      <div
-                        style={{
-                          alignSelf: "flex-end",
-                          height: 15,
-                          width: 15,
-                          borderRadius: 10,
-                          backgroundColor: project.abandoned
-                            ? "red"
-                            : "#1EA362",
-                        }}
-                      ></div>
-                    </div>
-                  </div>
-
-                  <div
-                    className="mb-1"
-                    style={{
-                      display: "flex",
-                      justifyContent: "start",
-                    }}
-                  >
-                    <span
-                      style={{
-                        fontSize: "0.8rem",
-                        color: "gray",
-                        marginRight: "10px",
-                      }}
-                    >
-                      {project.date}
-                    </span>
-                    <span
-                      style={{
-                        fontSize: "0.8rem",
-                        color: "gray",
-                        marginRight: "10px",
-                      }}
-                    >
-                      {project.status}
-                    </span>
-                  </div>
-                  <div
-                    className="mb-1"
-                    style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "start",
-                    }}
-                  >
-                    <span
-                      style={{
-                        fontSize: "0.8rem",
-                        color: "gray",
-                        marginRight: "10px",
-                      }}
-                    >
-                      {project.contractor}
-                    </span>
-                    <span
-                      style={{
-                        fontSize: "0.8rem",
-                        color: "gray",
-                        marginRight: "10px",
-                      }}
-                    >
-                      {project.location}
-                    </span>
-                  </div>
-                </div>
+                <p>No Data found</p>
               </div>
             </li>
-          ))}
+          )}
         </ul>
       </div>
     </aside>
